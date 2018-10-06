4f902ed9ea62e1df9704dd4da335e4fc99846653
pasipasi123 <pasi.haapakorva@gmail.com>
2018-10-06 20:23:23 +0300

    Tidying the code

--- 00 excelidata.R
+++ 00 excelidata.R
@@ -1,45 +1,9 @@
-?readxl::read_excel
-
 library(tidyverse)
 
-matkat <- readxl::read_excel("C:/Users/pasih_000/Documents/rprojektit/kulkutavatvyoh/data/Oulun_seutu_tulokset.xlsx", "D121", 
-                   skip = 11, n_max = 10)
-
-suoritteet <- readxl::read_excel("C:/Users/pasih_000/Documents/rprojektit/kulkutavatvyoh/data/Oulun_seutu_tulokset.xlsx", "D121", 
-                             skip = 25, n_max = 10)
-
-matkat <- matkat %>% 
-   set_names("vyohyke", "kk", "pp", "jk", "ha1", "ha2", "muu", "kaikki") %>% 
-   filter(!is.na(vyohyke)) %>% 
-   mutate_at(vars("ha1", "ha2"), as.numeric) %>% 
-   transmute(vyohyke, kk, pp, jk, ha = ha1 + ha2, muu, kaikki)
-
-suoritteet <- suoritteet %>% 
-   set_names("vyohyke", "kk", "pp", "jk", "ha1", "ha2", "muu", "kaikki") %>% 
-   filter(!is.na(vyohyke)) %>% 
-   mutate_at(vars("ha1", "ha2"), as.numeric) %>% 
-   transmute(vyohyke, kk, pp, jk, ha = ha1 + ha2, muu, kaikki)
-
-oulukuva <- matkat %>% 
-   mutate(vyohyke = isoksi(vyohyke) %>% as_factor()) %>% 
-   gather(kt, osuus, kk:muu) %>% 
-   mutate(kt = as_factor(kt)) %>% 
-   mutate(osuus = osuus / kaikki) %>% 
-   ggplot(aes(vyohyke %>% fct_rev(), osuus, fill = kt %>% fct_rev())) +
-   geom_col() +
-   coord_flip() +
-   geom_hline(yintercept = c(0.25, 0.5, 0.75), lty = 3, color = "gray50") +
-   geom_text(aes(label = format(round(osuus * 100, 1), decimal.mark = ",")), 
-             position = position_stack(vjust = 0.5), size = 3) +
-   scale_fill_brewer(palette = "Set2", name = NULL, labels = rev(c("Jalankulku", "Pyöräily", "Joukkoliikenne", "Henkilöauto", "Muu"))) +
-   scale_y_continuous(labels = function(x) paste(x * 100, "%")) +
-   guides(fill = guide_legend(reverse = TRUE)) +
-   theme(legend.position = "top") +
-   labs(x = NULL, y = NULL)
-
-leg1 <- cowplot::get_legend(oulukuva)
-
-ggsave("kto_vyohyke.png", h = 4, w = 7)
+isoksi <- function(x) {
+   y <- str_sub(x, 1, 1)
+   paste0(toupper(y), str_sub(x, 2, nchar(x)))
+}
 
 # tehtailu ----
 
@@ -47,51 +11,31 @@ files <- list.files("data/hlt/", full.names = TRUE)
 
 nimet <- c("vyohyke", "kk", "pp", "jk", "ha1", "ha2", "muu", "kaikki")
 
-data_files <- map(files, ~ readxl::read_excel(., sheet = "D121", skip = 14, n_max = 10, col_names = FALSE))
+data_files <- map(files, readxl::read_excel, sheet = "D121", skip = 14, n_max = 10, col_names = FALSE)
 
-data_nest <- map(data_files, as_tibble) %>% 
+hlt_seutu <- map(data_files, as_tibble) %>% 
    map(set_names, nimet) %>%
    set_names(tools::file_path_sans_ext(basename(files))) %>% 
-   map(~ mutate(., vyohyke = isoksi(vyohyke))) %>% 
-   map(~ filter(., !is.na(kaikki))) %>% 
-   map(~ transmute(., vyohyke, kk, pp, jk, ha = ha1 + ha2, muu, kaikki)) %>% 
+   map(mutate, vyohyke = isoksi(vyohyke)) %>% 
+   map(filter, !is.na(kaikki)) %>% 
+   map(transmute, vyohyke, kk, pp, jk, ha = ha1 + ha2, muu, kaikki) %>% 
    enframe() %>% 
-   mutate(name = isoksi(name))
-
-kuvafun <- function(data, title) {
-   data %>% 
-     mutate(vyohyke = as_factor(vyohyke)) %>% 
-     gather(kt, osuus, kk:muu) %>% 
-     mutate(kt = as_factor(kt)) %>% 
-     mutate(osuus = osuus / kaikki) %>% 
-     ggplot(aes(vyohyke %>% fct_rev(), osuus, fill = kt %>% fct_rev())) +
-     geom_col() +
-     coord_flip() +
-     geom_hline(yintercept = c(0.25, 0.5, 0.75), lty = 3, color = "gray50") +
-     geom_text(aes(label = format(round(osuus * 100, 1), decimal.mark = ",")), 
-               position = position_stack(vjust = 0.5), size = 3) +
-     scale_fill_brewer(palette = "Set2", name = NULL, labels = rev(c("Jalankulku", "Pyöräily", "Joukkoliikenne", "Henkilöauto", "Muu"))) +
-     scale_y_continuous(labels = function(x) paste(x * 100, "%")) +
-     guides(fill = guide_legend(reverse = TRUE)) +
-     theme(legend.position = "none") +
-     labs(x = NULL, y = NULL, title = title)
-}
-
-kuva_nest <- data_nest %>% 
-   mutate(kuva = map2(value, name, kuvafun)) 
-
-nestp1 <- data_nest %>% 
+   mutate(name = isoksi(name)) %>% 
    unnest() %>% 
    mutate(name = str_replace(name, "_tulokset", ""),
           name = str_replace(name, "_", " "),
           name = str_replace(name, "ät Hä", "ät-Hä")) %>% 
    mutate(vyohyke = as_factor(vyohyke)) %>% 
+   mutate(vyohyke = fct_relevel(vyohyke, "Koko seutu", after = 7)) %>% 
    gather(kt, osuus, kk:muu) %>% 
    mutate(kt = as_factor(kt)) %>% 
-   mutate(osuus = osuus / kaikki) %>% 
-   filter(!str_detect(vyohyke, "oko")) %>% 
+   mutate(osuus = osuus / kaikki)
+
+
+nestp1 <- hlt_seutu %>%
+   # filter(!str_detect(vyohyke, "oko")) %>%
    ggplot(aes(vyohyke %>% fct_rev(), osuus, fill = kt %>% fct_rev())) +
-   facet_wrap(~ name) +
+   facet_wrap(~ name, ncol = 2, scales = "free_y") +
    geom_col() +
    coord_flip() +
    geom_hline(yintercept = c(0.25, 0.5, 0.75), lty = 3, color = "gray50") +
@@ -107,11 +51,42 @@ nestp1 <- data_nest %>%
    theme(legend.position = "top") +
    labs(x = NULL, y = NULL) 
 
-ggsave("nest_kuva.png", h = 10, w = 13)
+ggsave("nest_kuva.png", h = 11, w = 7)
 
-kuvat <- kuva_nest %>% pull(kuva)
+jarj_d <- data_nest %>% 
+   unnest() %>% 
+   mutate(name = str_replace(name, "_tulokset", ""),
+          name = str_replace(name, "_", " "),
+          name = str_replace(name, "ät Hä", "ät-Hä")) %>% 
+   mutate(vyohyke = as_factor(vyohyke)) %>% 
+   gather(kt, osuus, kk:muu) %>% 
+   mutate(kt = as_factor(kt)) %>% 
+   mutate(osuus = osuus / kaikki) %>% 
+   filter(!str_detect(vyohyke, "oko")) %>%
+   filter(kt == "pp") %>% 
+   # complete(name, vyohyke, fill = list(osuus = 0)) %>%
+   mutate(x_lab = osuus) %>% 
+   filter(!is.na(x_lab)) %>% 
+   arrange(vyohyke, x_lab) %>% 
+   mutate(x_lab = as.character(x_lab)) %>% 
+   filter(!str_detect(vyohyke, "oko"))
 
-grid1 <- cowplot::plot_grid(plotlist = kuva_nest$kuva)
-grid2 <- cowplot::plot_grid(leg1, grid1, ncol = 1, rel_heights = c(1, 10))
+pp_vari <- RColorBrewer::brewer.pal(5, "Set2")[4]
 
-ggsave("isokuva.png", h = 15, w = 20)
+jarj_d %>% 
+   ggplot(aes(x_lab, osuus, fill = "heh")) +
+   facet_wrap(~ vyohyke, ncol = 2, scales = "free_y") +
+   scale_x_discrete(labels = jarj_d$name, breaks = jarj_d$x_lab) +
+   geom_col(show.legend = FALSE) +
+   coord_flip() +
+   scale_fill_manual(values = pp_vari) +
+   scale_y_continuous(labels = function(x) paste(x * 100, "%"), expand = expand_scale(c(0.05, 0.1))) +
+   geom_text(aes(y = osuus + 0.02, label = format(round(osuus * 100, 1), decimal.mark = ",")), size = 2.5) +
+   theme_minimal() +
+   theme(panel.background = element_rect(color = "gray90", fill = "white"),
+         panel.grid.major.y = element_blank(),
+         text = element_text(size = 10)) +
+   labs(x = NULL, y = NULL) 
+
+ggsave("pyorailyn_osuus.png", h = 6, w = 7)
+
